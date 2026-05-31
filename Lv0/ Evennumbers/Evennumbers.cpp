babfcfacf91d3d1bc5fb2e3acd0d3809b9e79976
L_CJ76 <dlckdwls76@gmail.com>
2026-05-31 18:49:33 +0900

    Lv0_Coding
    Lv0_Coding

---
+++ Lv0/ Evennumbers/Evennumbers.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+#include <windows.h>
+
+using namespace std;
+
+// 코딩테스트 문제의 함수
+vector<int> solution(int n) {
+    vector<int> answer;
+    
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 == 1) { 
+            answer.push_back(i); 
+        }
+    }
+    
+    return answer;
+}
+
+// 로컬 테스트를 위한 main 함수
+int main() {
+    // 테스트 케이스 1: n = 10
+    int n1 = 10;
+    vector<int> result1 = solution(n1);
+    
+    cout << "n이 " << n1 << "일 때 결과: [";
+    for(int i = 0; i < result1.size(); i++) {
+        cout << result1[i];
+        if (i != result1.size() - 1) cout << ", ";
+    }
+    cout << "]" << endl;
+
+    // 테스트 케이스 2: n = 15
+    int n2 = 15;
+    vector<int> result2 = solution(n2);
+    
+    cout << "n이 " << n2 << "일 때 결과: [";
+    for(int i = 0; i < result2.size(); i++) {
+        cout << result2[i];
+        if (i != result2.size() - 1) cout << ", ";
+    }
+    cout << "]" << endl;
+
+    return 0;
+}
---
+++ Lv0/Lv0_SharePizza_2/Lv0_SharePizza_2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <windows.h> // 한글 깨짐 방지를 위한 Windows API
+
+using namespace std;
+
+// solution 함수는 반드시 한 번만 작성해야 합니다.
+int solution(int n) {
+    int pizza = 1;
+    
+    while ((pizza * 6) % n != 0) {
+        pizza++;
+    }
+    
+    return pizza;
+}
+
+int main() {
+    // 콘솔창 출력 인코딩을 UTF-8로 강제 설정 (한글 깨짐 방지)
+    SetConsoleOutputCP(CP_UTF8);
+
+    // 문제에 주어진 입출력 예시 테스트
+    int test1 = solution(6);
+    int test2 = solution(10);
+    int test3 = solution(4);
+    
+    // 결과 출력
+    cout << "입출력 예 #1 결과: " << test1 << " (기댓값: 1)" << "\n";
+    cout << "입출력 예 #2 결과: " << test2 << " (기댓값: 5)" << "\n";
+    cout << "입출력 예 #3 결과: " << test3 << " (기댓값: 2)" << "\n";
+    
+    return 0;
+}
